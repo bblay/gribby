6035a7b644e052b2850d3238e01512fb24652020
bblay <byronblay@googlemail.com>
2013-05-03 16:48:43 +0100

    wip

--- lib/gribby/__init__.py
+++ lib/gribby/__init__.py
@@ -7,4 +7,27 @@ https://software.ecmwf.int/wiki/display/GRIB/Releases
 
 """
 
+import ctypes
+
+
 __version__ = 0.1
+
+
+so = ctypes.CDLL("/data/local/itbb/grib/grib_api-1.10.0/lib/libgrib_api.so")
+
+print so
+
+print so.grib_samples_new_from_file("GRIB2")
+
+def read(file):
+    
+    if isinstance(file, basestring):
+        file = open(file, "rb")
+        
+#     while True:
+#         so.
+        
+        
+        
+        
+# new function in 1.10.0: grib_is_defined
